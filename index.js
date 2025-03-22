3be92e7ddd2e3d95cb2159e9eff58e21f9490744
Carla Sousa <sousa.carla09@gmail.com>
2025-03-22 20:37:37 -0300

    feat: remove one element from tree

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function insert(root, value) {
   return root;
 }
 
-// 3. Create binary tree from an array
+// create binary tree from an array
 function buildTree(values) {
   let root = null;
   for (let val of values) {
@@ -29,4 +29,40 @@ function buildTree(values) {
   return root;
 }
 
-console.log(buildTree([10, 5, 15, 3, 7, 12, 18]));
+// remove an element keeping the BST standard
+function findMin(node) {
+  const minimum = node.value;
+  while (node.left !== null) {
+    minimum = node.left.value;
+    node = node.left;
+  }
+  return minimum;
+}
+
+function remove(root, value) {
+  if (root === null) return root;
+
+  if (value < root.value) {
+    root.left = remove(root.left, value);
+  } else if (value > root.value) {
+    root.right = remove(root.right, value);
+  } else {
+    // case node is if no children 
+    if (root.left === null && root.right === null) {
+      return null;
+    }
+
+    // case node is if a single child
+    if (root.left === null) return root.right;
+    if (root.right === null) return root.left;
+
+    // case node is if two children
+    root.value = findMin(root.right);
+    root.right = remove(root.right, root.value);
+  }
+
+  return root;
+}
+
+const tree = buildTree([10, 5, 15, 3, 7, 12, 18]);
+console.log(remove(tree, 15));
